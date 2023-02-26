8e1d311f05f4eb83dc3f3b94d44267f1476b7a98
Oraegbu Charles <oraegbucharlesc@gmail.com>
2023-02-26 23:44:49 +0100

    edit distance

---
+++ go/randoms/edit_distance.go
@@ -0,0 +1,33 @@
+package randoms
+
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+// You have the following three operations permitted on a word:
+
+// Insert a character
+// Delete a character
+// Replace a character
+
+func Ministance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i <= m; i++ {
+		dp[i][0] = i
+	}
+	for j := 0; j <= n; j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+			}
+		}
+	}
+	return dp[m][n]
+}
